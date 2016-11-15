3ac88b5f1c8c98cc7a5ac3c121c1921e2371234a
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2016-11-15 17:39:31 +0800

    Final commit

---
+++ src/MovieQuote.java
@@ -0,0 +1,6 @@
+public class MovieQuote {
+
+    public static void main(String[] args) {
+        System.out.println("The name's Plissken!");
+    }
+}
---
+++ src/MovieQuoteInfo.java
@@ -0,0 +1,9 @@
+public class MovieQuoteInfo {
+
+    public static void main(String[] args) {
+        System.out.print("The name's Plissken!");
+        System.out.print(", Escape from New York");
+        System.out.print(", Snake Plissken");
+        System.out.print(", 1981.");
+    }
+}
